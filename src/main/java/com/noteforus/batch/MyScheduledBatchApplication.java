adcae7a4ecf961426cc14fbcbab99c994c12c591
Adam Lee <biltleee@gmail.com>
2019-09-05 20:05:18 +0900

    initial.

---
+++ src/main/java/com/noteforus/batch/MyScheduledBatchApplication.java
@@ -0,0 +1,13 @@
+package com.noteforus.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduledBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScheduledBatchApplication.class, args);
+	}
+
+}
